61bcef1f1f05e05be06712c0327be12663b2f696
cibi <cibi@motorq.com>
2023-08-21 19:21:26 -0700

    tooltip type fix

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -2,13 +2,15 @@
 
 import * as React from "react";
 import * as ToolTipPrimitive from "@radix-ui/react-tooltip";
+import { Tool } from "langchain/dist/tools/base";
 
 
-const ToolTip = (props) => (<ToolTipPrimitive.Provider>
+
+const ToolTip = (props: ToolTipPrimitive.TooltipProps) => (<ToolTipPrimitive.Provider>
         <ToolTipPrimitive.Root  {...props}></ToolTipPrimitive.Root>
         </ToolTipPrimitive.Provider>);
-const ToolTipTrigger = (props) => (<ToolTipPrimitive.Trigger asChild  {...props}/>);
-const ToolTipContent = ({ children, ...props }) => (<ToolTipPrimitive.Portal>
+const ToolTipTrigger = (props: ToolTipPrimitive.TooltipTriggerProps) => (<ToolTipPrimitive.Trigger asChild  {...props}/>);
+const ToolTipContent = ({ children, ...props }: ToolTipPrimitive.TooltipContentProps) => (<ToolTipPrimitive.Portal>
                                         <ToolTipPrimitive.Content sideOffset={10} side="bottom" align="center"  alignOffset={10} {...props} className="text-sm">
                                             {children}
                                         </ToolTipPrimitive.Content>
